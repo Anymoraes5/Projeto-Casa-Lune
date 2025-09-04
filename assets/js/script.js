6ce33966465cc54758f8f3b18112ee8747e67186
Ana Caroline Moraes <166078471+Anymoraes5@users.noreply.github.com>
2025-09-04 17:54:18 -0300

    [feat] responsividade 100% funcional

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,15 +28,27 @@ function slideNext() {
 
 setInterval(slideNext, 2000);
 //--------------------ver mais/catalogo------------------
-document.addEventListener('DOMContentLoaded', () => {
-    const btn = document.getElementById('ver-mais');
-    const extras = document.querySelectorAll('#grid-catalogo .extra');
+const botao = document.getElementById("ver-mais");
+const itensExtras = document.querySelectorAll("#grid-catalogo .extra");
 
-    btn.addEventListener('click', () => {
-        const mostrar = !extras[0].classList.contains('show');
-
-        extras.forEach(item => item.classList.toggle('show', mostrar));
-
-        btn.textContent = mostrar ? 'Ver menos' : 'Ver mais';
+botao.addEventListener("click", () => {
+    itensExtras.forEach((item, index) => {
+        setTimeout(() => {
+            item.classList.toggle("show");
+        }, index * 150); // cada item aparece 150ms depois do anterior
     });
+
+    botao.textContent = botao.textContent === "Ver mais" ? "Ver menos" : "Ver mais";
 });
+//===========================fade=======================
+const imgs = document.querySelectorAll("#fade-js .fade-img");
+let current = 0;
+
+function nextImage() {
+  imgs[current].classList.remove("active");
+  current = (current + 1) % imgs.length;
+  imgs[current].classList.add("active");
+}
+
+imgs[0].classList.add("active"); // inicial
+setInterval(nextImage, 3000); // troca a cada 3 segundos
